9e955bc196a0c0ed75660a616c55b02e7ba4b93e
Dang-Thanh Tran <trandgthanh@gmail.com>
2019-10-16 21:48:34 +0200

    removed whitespaces, ready before adding RailsAdmin

--- devise_railsadmin.rb
+++ devise_railsadmin.rb
@@ -3,34 +3,34 @@ run 'pgrep spring | xargs kill -9'
 # GEMFILE
 run 'rm Gemfile'
 file 'Gemfile', <<-RUBY
-  source 'https://rubygems.org'
-  ruby '#{RUBY_VERSION}'
-  #{"gem 'bootsnap', require: false" if Rails.version >= "5.2"}
-  gem 'jbuilder', '~> 2.0'
-  gem 'pg', '~> 0.21'
-  gem 'puma'
-  gem 'rails', '#{Rails.version}'
-  gem 'redis'
-  gem 'autoprefixer-rails'
-  gem 'font-awesome-sass', '~> 5.6.1'
-  gem 'sassc-rails'
-  gem 'simple_form'
-  gem 'uglifier'
-  gem 'webpacker'
-  gem 'devise'
+source 'https://rubygems.org'
+ruby '#{RUBY_VERSION}'
+#{"gem 'bootsnap', require: false" if Rails.version >= "5.2"}
+gem 'jbuilder', '~> 2.0'
+gem 'pg', '~> 0.21'
+gem 'puma'
+gem 'rails', '#{Rails.version}'
+gem 'redis'
+gem 'autoprefixer-rails'
+gem 'font-awesome-sass', '~> 5.6.1'
+gem 'sassc-rails'
+gem 'simple_form'
+gem 'uglifier'
+gem 'webpacker'
+gem 'devise'
 
-  group :development do
-    gem 'web-console', '>= 3.3.0'
-  end
+group :development do
+  gem 'web-console', '>= 3.3.0'
+end
 
-  group :development, :test do
-    gem 'pry-byebug'
-    gem 'pry-rails'
-    gem 'listen', '~> 3.0.5'
-    gem 'spring'
-    gem 'spring-watcher-listen', '~> 2.0.0'
-    gem 'dotenv-rails'
-  end
+group :development, :test do
+  gem 'pry-byebug'
+  gem 'pry-rails'
+  gem 'listen', '~> 3.0.5'
+  gem 'spring'
+  gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'dotenv-rails'
+end
 RUBY
 
 # Ruby versio
@@ -53,19 +53,19 @@ file '.ruby-version', RUBY_VERSION
 # Database conf fil
 inside 'config' do
   database_conf = <<-EOF
-    default: &default
-      adapter: postgresql
-      encoding: unicode
-      pool: <%= ENV.fetch("RAILS_MAX_THREADS") { 5 } %>
-    development:
-      <<: *default
-      database: #{app_name}_development
-    test:
-      <<: *default
-      database: #{app_name}_test
-    production:
-      <<: *default
-      url: <%= ENV['POSTGRESQL_ADDON_URI'] %>
+default: &default
+  adapter: postgresql
+  encoding: unicode
+  pool: <%= ENV.fetch("RAILS_MAX_THREADS") { 5 } %>
+development:
+  <<: *default
+  database: #{app_name}_development
+test:
+  <<: *default
+  database: #{app_name}_test
+production:
+  <<: *default
+  url: <%= ENV['POSTGRESQL_ADDON_URI'] %>
   EOF
   file 'database.yml', database_conf, force: true
 end
@@ -79,14 +79,14 @@ run 'rm app/assets/stylesheets/components/_avatar.scss'
 run 'rm app/assets/stylesheets/components/_navbar.scss'
 run 'rm app/assets/stylesheets/components/_index.scss'
 file 'app/assets/stylesheets/components/_index.scss', <<-SCSS
-  // Import your components CSS files here.
-  @import "alert";
+// Import your components CSS files here.
+@import "alert";
 SCSS
 
 run 'rm app/assets/javascripts/application.js'
 file 'app/assets/javascripts/application.js', <<-JS
-  //= require rails-ujs
-  //= require_tree .
+//= require rails-ujs
+//= require_tree .
 JS
 
 # Dev environmen
@@ -95,48 +95,48 @@ gsub_file('config/environments/development.rb', /config\.assets\.debug.*/, 'conf
 # Layou
 run 'rm app/views/layouts/application.html.erb'
 file 'app/views/layouts/application.html.erb', <<-HTML
-  <!DOCTYPE html>
-  <html>
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <title>TODO</title>
-      <%= csrf_meta_tags %>
-      <%= action_cable_meta_tag %>
-      <%= stylesheet_link_tag 'application', media: 'all' %>
-      <%#= stylesheet_pack_tag 'application', media: 'all' %> <!-- Uncomment if you import CSS in app/javascript/packs/application.js -->
-    </head>
-    <body>
-      <%= render 'shared/flashes' %>
-      <%= yield %>
-      <%= javascript_include_tag 'application' %>
-      <%= javascript_pack_tag 'application' %>
-    </body>
-  </html>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>TODO</title>
+    <%= csrf_meta_tags %>
+    <%= action_cable_meta_tag %>
+    <%= stylesheet_link_tag 'application', media: 'all' %>
+    <%#= stylesheet_pack_tag 'application', media: 'all' %> <!-- Uncomment if you import CSS in app/javascript/packs/application.js -->
+  </head>
+  <body>
+    <%= render 'shared/flashes' %>
+    <%= yield %>
+    <%= javascript_include_tag 'application' %>
+    <%= javascript_pack_tag 'application' %>
+  </body>
+</html>
 HTML
 
 file 'app/views/shared/_flashes.html.erb', <<-HTML
-  <% if notice %>
-    <div class="alert alert-info alert-dismissible fade show m-1" role="alert">
-      <%= notice %>
-      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-      </button>
-    </div>
-  <% end %>
-  <% if alert %>
-    <div class="alert alert-warning alert-dismissible fade show m-1" role="alert">
-      <%= alert %>
-      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-      </button>
-    </div>
-  <% end %>
+<% if notice %>
+  <div class="alert alert-info alert-dismissible fade show m-1" role="alert">
+    <%= notice %>
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
+<% end %>
+<% if alert %>
+  <div class="alert alert-warning alert-dismissible fade show m-1" role="alert">
+    <%= alert %>
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
+<% end %>
 HTML
 
 # READM
 markdown_file_content = <<-MARKDOWN
-  Rails app generated based on the awesome [lewagon/rails-templates](https://github.com/lewagon/rails-templates) ([Le Wagon coding bootcamp](https://www.lewagon.com)).
+Rails app generated based on the awesome [lewagon/rails-templates](https://github.com/lewagon/rails-templates) ([Le Wagon coding bootcamp](https://www.lewagon.com)).
 MARKDOWN
 file 'README.md', markdown_file_content, force: true
 
@@ -162,11 +162,11 @@ after_bundle do
 
   # Git ignore
   append_file '.gitignore', <<-TXT
-    # Ignore .env file containing credentials.
-    .env*
-    # Ignore Mac and Linux file system files
-    *.swp
-    .DS_Store
+# Ignore .env file containing credentials.
+.env*
+# Ignore Mac and Linux file system files
+*.swp
+.DS_Store
   TXT
 
   # Devise install + user
@@ -176,10 +176,10 @@ after_bundle do
   # App controller
   run 'rm app/controllers/application_controller.rb'
   file 'app/controllers/application_controller.rb', <<-RUBY
-    class ApplicationController < ActionController::Base
-      protect_from_forgery with: :exception
-      before_action :authenticate_user!
-    end
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :authenticate_user!
+end
   RUBY
 
   # migrate + devise views
@@ -189,11 +189,11 @@ after_bundle do
   # Pages Controller
   run 'rm app/controllers/pages_controller.rb'
   file 'app/controllers/pages_controller.rb', <<-RUBY
-    class PagesController < ApplicationController
-      skip_before_action :authenticate_user!, only: [:home]
-      def home
-      end
-    end
+class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:home]
+  def home
+  end
+end
   RUBY
 
   # Environments
@@ -204,21 +204,21 @@ after_bundle do
   run 'rm app/javascript/packs/application.js'
   run 'yarn add popper.js jquery bootstrap'
   file 'app/javascript/packs/application.js', <<-JS
-    import "bootstrap";
+import "bootstrap";
   JS
 
   inject_into_file 'config/webpack/environment.js', before: 'module.exports' do <<-JS
-    const webpack = require('webpack')
-    // Preventing Babel from transpiling NodeModules packages
-    environment.loaders.delete('nodeModules');
-    // Bootstrap 4 has a dependency over jQuery & Popper.js:
-    environment.plugins.prepend('Provide',
-      new webpack.ProvidePlugin({
-        $: 'jquery',
-        jQuery: 'jquery',
-        Popper: ['popper.js', 'default']
-      })
-    );
+const webpack = require('webpack')
+// Preventing Babel from transpiling NodeModules packages
+environment.loaders.delete('nodeModules');
+// Bootstrap 4 has a dependency over jQuery & Popper.js:
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
+  })
+);
   JS
   end
 
@@ -228,10 +228,10 @@ after_bundle do
 
 end
 
-# Ruboco
+# Rubocop
 run 'curl -L https://raw.githubusercontent.com/lewagon/rails-templates/master/.rubocop.yml > .rubocop.yml'
 
-# Gi
+# Git
 git :init
 git add: '.'
-git commit: "-m 'Initial commit with devise template from https://github.com/lewagon/rails-templates'"
+git commit: "-m 'Initial commit with devise & rails admin template'"
