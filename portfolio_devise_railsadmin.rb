c0e7a38dc074a5561374632237a9507e76ffb24a
Dang-Thanh Tran <trandgthanh@gmail.com>
2019-10-17 11:08:31 +0200

    configured rails admin initializer file

--- portfolio_devise_railsadmin.rb
+++ portfolio_devise_railsadmin.rb
@@ -217,6 +217,54 @@ end
   rails_command 'db:migrate'
 
   generate('rails_admin:install')
+
+  run 'rm config/initializers/rails_admin.rb'
+  file 'config/initializers/rails_admin.rb', <<-RUBY
+RailsAdmin.config do |config|
+  config.authorize_with do |controller|
+    redirect_to main_app.root_path unless current_user && current_user.admin
+  end
+
+  ### Popular gems integration
+
+  ## == Devise ==
+  # config.authenticate_with do
+  #   warden.authenticate! scope: :user
+  # end
+  # config.current_user_method(&:current_user)
+
+  ## == CancanCan ==
+  # config.authorize_with :cancancan
+
+  ## == Pundit ==
+  # config.authorize_with :pundit
+
+  ## == PaperTrail ==
+  # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
+
+  ### More at https://github.com/sferik/rails_admin/wiki/Base-configuration
+
+  ## == Gravatar integration ==
+  ## To disable Gravatar integration in Navigation Bar set to false
+  # config.show_gravatar = true
+
+  config.actions do
+    dashboard                     # mandatory
+    index                         # mandatory
+    new
+    export
+    bulk_delete
+    show
+    edit
+    delete
+    show_in_app
+
+    ## With an audit adapter, you can add:
+    # history_index
+    # history_show
+  end
+end
+  RUBY
   # RAILS ADMIN
 
 
